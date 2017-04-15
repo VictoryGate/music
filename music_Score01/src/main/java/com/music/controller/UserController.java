169f550c408b616958895946399d6e8e6be9c5ab
Gavinagao <1490925134@qq.com>
2017-04-15 15:42:26 +0800

    modify user login url

--- music_Score01/src/main/java/com/music/controller/UserController.java
+++ music_Score01/src/main/java/com/music/controller/UserController.java
@@ -2,8 +2,8 @@
       
     import java.util.List;  
       
-    import org.apache.log4j.Logger;  
-    import org.springframework.beans.factory.annotation.Autowired;  
+    import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;  
     import org.springframework.stereotype.Controller;  
     import org.springframework.ui.ModelMap;  
     import org.springframework.web.bind.annotation.PathVariable;  
